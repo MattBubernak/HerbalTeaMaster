11aad5b2129d12e4d49177da55914e528476aafb
unknown <matt.bubernak@gmail.com>
2015-12-28 23:21:48 -0700

    ingredient list controller getting data

--- public/javascripts/controllers/IngredientCtrl.js
+++ public/javascripts/controllers/IngredientCtrl.js
@@ -1,8 +1,23 @@
 /**
  * Created by socce on 12/28/2015.
  */
-angular.module('IngredientCtrl', []).controller('IngredientController', function($scope) {
+angular.module('IngredientCtrl', []).controller('IngredientController', function($scope,$http) {
 
-    $scope.tagline = 'Nothing beats a pocket protector!';
+    var app = this;
+    var url = "http://localhost:3000";
+
+    app.saveIngredient = function (newName,newDescription)
+    {
+        $http.post("http://localhost:3000/ingredient", {name:newName,description:newDescription}).success(function () {
+            loadIngredients();
+        })
+    }
+    function loadIngredients() {
+        //app.ingredients = [{name:"hi",description:"bye"}];
+        $http.get('http://localhost:3000/ingredient').success(function (ingredients) {
+            app.ingredients = ingredients;
+        }).error(function () { app.ingredients = [{name:"Faild to load ingredients"}]})
+    }
+    loadIngredients();
 
 });
